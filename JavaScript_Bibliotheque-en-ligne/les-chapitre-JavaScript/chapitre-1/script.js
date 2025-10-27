48321a66cf4d14c5a74ca6039c37482bd52d90bc
soulayman-7x <idbahasevenx@gmail.com>
2025-10-27 08:32:04 +0000

    add filles of JavaScript_Bibliotheque-en-ligne

---
+++ JavaScript_Bibliotheque-en-ligne/les-chapitre-JavaScript/chapitre-1/script.js
@@ -0,0 +1,5 @@
+let text = document.getElementById("titre-principal");
+console.log("texte de titre : " + text.innerText);
+
+let button = document.querySelector(".btn");
+console.log("texte de button : " + button.innerText);
